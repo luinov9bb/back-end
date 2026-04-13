123609eddbb65f0bb45b9afff1adb3a6a56e0ee8
Yevhenii N <anaconda79star@gmail.com>
2026-04-13 23:50:50 +0300

    v0 project created

---
+++ bookStore.Api/Controller/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookStore.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
